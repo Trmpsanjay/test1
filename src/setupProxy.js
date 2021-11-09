b95cfe00021aa7f6c10f3b236f11d27aa3d3ec88
trmpsanjay <trmpsanjay@gmail.com>
2021-11-09 14:56:31 +0530

    d

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://10.10.10.189:8000',
+      target: 'http://10.10.10.189:8000//api/v1',
       changeOrigin: true,
     })
   );
