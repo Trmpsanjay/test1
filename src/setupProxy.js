5011b1dd38ecb7443980cfef434b75ea5ce41909
trmpsanjay <trmpsanjay@gmail.com>
2021-11-09 14:39:34 +0530

    cors issue

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const createProxyMiddleware = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://10.10.10.189:8000',
+      changeOrigin: true,
+    })
+  );
+};
